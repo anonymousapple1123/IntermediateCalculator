431dbe5e84e87bcf660f2ca636c4482f73f3053d
Ayush Kumar <ayush.kmr1123@gmail.com>
2025-01-30 10:21:52 +0530

    rearranged the structure of files

--- src/complex_number_arithmetics.cpp
+++ src/complex_number_arithmetics.cpp
@@ -39,16 +39,4 @@ std::vector<T> SubtractComplexNumber(){
 
 };
 
-int main(){
-std::cout<<"_____________________________________________________PROGRAM STARTED................................................"<<std::endl;
-//std::cout<<"Enter first number :_"<<std::endl;
-//std::cin>>r_1,i_1;
-//std::cin>>r_2,i_2;
-ComplexNumber<double> num1(1.0,9.0,4.0,1.0);
-	
-std::vector <double> array = num1.AddComplexNumber();
-
-std::cout<<"Resulatant vecctor "<<array[0]<<"  i"<<array[1]<<std::endl;
-return 0;
-}
 
