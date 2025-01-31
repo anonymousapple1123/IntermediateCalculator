5d9906395cee951bbb5986718e5b0d4876342860
Ayush Kumar <ayush.kmr1123@gmail.com>
2025-01-31 10:56:15 +0530

    edited some few lines(barely any changes)

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,68 @@
-#include<iostream>
-#include"complex_number_arithmetics.cpp"
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include "complex_number_arithmetics.h"
 
-int main(){
-std::cout<<"_____________________________________________________PROGRAM STARTED................................................"<<std::endl;
-//std::cout<<"Enter first number :_"<<std::endl;
-//std::cin>>r_1,i_1;
-//std::cin>>r_2,i_2;
-ComplexNumber<double> num1(1.0,9.0,4.0,1.0);
-	
-std::vector <double> array = num1.AddComplexNumber();
+inline std::vector<double> parse_complex(const std::string& complex_str) {
+    std::string command = "./complex_parser \"" + complex_str + "\"";
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        std::cerr << "Failed to open pipe to executable." << std::endl;
+        return {};
+    }
+    char buffer[128];
+    std::string result = "";
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        result += buffer;
+    }
+    int status = pclose(pipe);
+    if (status != 0) {
+        std::cerr << "Executable returned an error." << std::endl;
+        return {};
+    }
+    double a, b;
+    if (sscanf(result.c_str(), "%lf,%lf", &a, &b) == 2) {
+        return {a, b};
+    } else {
+        std::cerr << "Failed to parse output from executable." << std::endl;
+        return {};
+    }
+}
 
-std::cout<<"Resulatant vecctor "<<array[0]<<"  i"<<array[1]<<std::endl;
-return 0;
+int main() {
+    std::vector<double> result_vec;
+    std::string number_1, number_2;
+
+    std::cout << "Enter first number : ";
+    if (!(std::getline(std::cin, number_1))) {
+        std::cerr << "Failed to read input." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Enter second number : ";
+    if (!(std::getline(std::cin, number_2))) {
+        std::cerr << "Failed to read input." << std::endl;
+        return 1;
+    }
+
+    std::vector<double> parsed_num1 = parse_complex(number_1);
+    std::vector<double> parsed_num2 = parse_complex(number_2);
+
+    if ((parsed_num1.size() == 2) && (parsed_num2.size() == 2)) {
+        ComplexNumber<double> complex(parsed_num1[0], parsed_num1[1], parsed_num2[0], parsed_num2[1]);
+        int choice = 1;
+        switch (choice) {
+        case 1:
+            result_vec = complex.AddComplexNumber();
+            std::cout << "= " << result_vec[0] << " + " << result_vec[1] << "i" << std::endl;
+            break;
+        default:
+            break;
+        }
+    } else {
+        std::cerr << "Failed to parse complex numbers." << std::endl;
+    }
+
+    return 0;
 }
