331e50d5b5b1cb86be54c32f5570dadde5dce781
Ayush Kumar <kumarayush9651@tutanota.com>
2025-02-04 01:23:18 -0800

    Update and enhanced the code removed unused files.

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,19 @@
 
 std::vector<double> parse_complex(std::string input) {
     double a, b;
-    // Remove spaces from the input
     input.erase(std::remove(input.begin(), input.end(), ' '), input.end());
 
-    // Find the position of the '+' or '-' sign for the imaginary part
     size_t plusPos = input.find('+');
     size_t minusPos = input.find('-');
 
-    // Determine the position of the imaginary part
+   
     size_t signPos = (plusPos != std::string::npos) ? plusPos : minusPos;
 
     if (signPos != std::string::npos) {
-        // Extract the real part (a)
         std::string realPart = input.substr(0, signPos);
         a = std::stod(realPart);
 
-        // Extract the imaginary part (b)
         std::string imaginaryPart = input.substr(signPos);
-        // Remove the 'i' character
         imaginaryPart.erase(std::remove(imaginaryPart.begin(), imaginaryPart.end(), 'i'), imaginaryPart.end());
         b = std::stod(imaginaryPart);
     } else {
@@ -36,8 +31,6 @@ std::vector<double> parse_complex(std::string input) {
     std::vector<double> result;
     result.push_back(a);
     result.push_back(b);
-    // std::cout << "Real part (a): " << a << std::endl;
-    // std::cout << "Imaginary part (b): " << b << std::endl;
 
     return result;
 }
@@ -67,7 +60,7 @@ int main() {
 
     if ((parsed_num1.size() == 2) && (parsed_num2.size() == 2)) {
         ComplexNumber<double> complex(parsed_num1[0], parsed_num1[1], parsed_num2[0], parsed_num2[1]);
-        int choice =3;
+        int choice =1;
         switch (choice) {
         case 1:
             result_vec = complex.AddComplexNumber();
