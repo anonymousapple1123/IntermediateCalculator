3e15a30d48f6cbdec3c31d045f680d3246d99a93
AyushKumarSingh <ayushkumar7355488975@gmail.com>
2025-02-05 19:25:00 +0530

    Added the power of i function along with modulus function

--- src/complex_number_arithmetics.h
+++ src/complex_number_arithmetics.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <type_traits>
 #include <vector>
+#include <cmath>
 
 template<typename T, typename Enable = void>
 class ComplexNumber;
@@ -52,6 +53,51 @@ public:
     result.push_back(imaginary_part);
     return result;
     }
+
+    std::vector<T> PowerOfI(int exponent){
+        std::vector<T> result;
+         if (!std::is_integral<decltype(exponent)>::value) {
+        std::cerr << "Error: Exponent must be an integer." << std::endl;
+        return result;
+        }
+        if (exponent < 0) {
+        std::cerr << "Error: Negative exponents are not supported for PowerOfI." << std::endl;
+        return result;
+        }
+        int cycle = exponent % 4;
+        if (cycle == 0) result.push_back(static_cast<T>(1));
+        else if (cycle == 1) {
+        result.push_back(static_cast<T>(0));
+        result.push_back(static_cast<T>(1));
+        }
+        else if (cycle == 2) result.push_back(static_cast<T>(-1));
+        else {
+        result.push_back(static_cast<T>(0));
+        result.push_back(static_cast<T>(-1));
+        }
+        return result;
+     /*
+        std::is_integral<T> is a C++ type trait that checks if T is an integer type.
+        decltype(exponent) ensures that we check the actual data type of exponent.
+        If exponent is not an integer, std::is_integral returns false.
+     */
+    }
+    
+std::vector<T>ConjugateComplexNumber() {
+    std::vector<T> result;
+    result.push_back(real_1);
+    result.push_back(-imaginary_1);
+    result.push_back(real_2);
+    result.push_back(-imaginary_2);
+    return result;
+}
+
+std::vector<T>ModulusComplexNumber() {
+    std::vector<T> result;
+    result.push_back(std::sqrt(real_1 * real_1 + imaginary_1 * imaginary_1));
+    result.push_back(std::sqrt(real_2 * real_2 + imaginary_2 * imaginary_2));
+    return result;
+}
 };
 
 #endif // COMPLEXNUMBER_H
